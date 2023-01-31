d4efd87d7fc185dfc660e050ff469a2cb9e2341a
hamhub7 <hamhub7@yahoo.com>
2023-01-31 09:14:32 -0500

    Initial commit

---
+++ src/main/java/net/hamhub7/tutorialmod/TutorialModClient.java
@@ -0,0 +1,12 @@
+package net.hamhub7.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient()
+    {
+
+    }
+}
